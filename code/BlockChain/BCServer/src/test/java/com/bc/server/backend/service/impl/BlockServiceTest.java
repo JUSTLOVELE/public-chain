05582b64b49191a067c5d5d9d0fa963a86df249b
JUSTLOVELE <694105388@qq.com>
2023-02-28 23:59:48 +0800

    add

--- code/BlockChain/BCServer/src/test/java/com/bc/server/backend/service/impl/BlockServiceTest.java
+++ code/BlockChain/BCServer/src/test/java/com/bc/server/backend/service/impl/BlockServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,6 +22,9 @@ public class BlockServiceTest {
 
     @Autowired
     private BlockChain _blockChain;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     public void writeToDiskTest() {
